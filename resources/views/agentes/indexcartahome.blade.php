0a014ee52f9d7ce835b9b0c374766e3b227f59d3
pericena12@gmail.com <pericena12@gmail.com>
2021-12-10 12:33:24 -0400

    cartilla

--- resources/views/agentes/indexcartahome.blade.php
+++ resources/views/agentes/indexcartahome.blade.php
@@ -14,17 +14,17 @@
     <title>Logistica</title>
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-    
+
     <!-- Bootstrap -->
-    <link type="text/css" rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}" >
+    <link type="text/css" rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}">
 
     <!-- Font Awesome Icon -->
     <link rel="stylesheet" href="{{asset('style/font-awesome.min.css')}}">
 
     <!-- Hoja de estilo personalizada -->
-    <link type="text/css" rel="stylesheet" href="{{asset('style/custom.css')}}" >
+    <link type="text/css" rel="stylesheet" href="{{asset('style/custom.css')}}">
 
-    <link rel="stylesheet" type="text/css" href="{{asset('styke/home.css')}}" >
+    <link rel="stylesheet" type="text/css" href="{{asset('styke/home.css')}}">
     <title> Home</title>
 </head>
 
@@ -32,141 +32,139 @@
 
     @foreach ($agentes as $agente)
 
-<<<<<<< HEAD
+    <<<<<<< HEAD <!-- product -->
+        <div class="col-md-3 col-xs-6">
+            <div class="product">
+                <div class="product-img">
+                    <img class="imgagente" src="{{asset($agente->url)}}" alt="Avatar">
+                    =======
+                    <div class="col-md-3 col-xs-6">
+                        <div class="product">
+                            <div class="product-img">
+                                <img class="imgagente img-fluid" src="{{asset($agente->url)}}" alt="">
 
-    <!-- product -->
-    <div class="col-md-3 col-xs-6">
-        <div class="product">
-            <div class="product-img">
-                <img class="imgagente" src="{{asset($agente->url)}}" alt="Avatar">
-=======
-    <div class="col-md-3 col-xs-6">
-        <div class="product">
-            <div class="product-img">
-                <img class="imgagente img-fluid" src="{{asset($agente->url)}}" alt="" >
->>>>>>> 8669d520c1228981195ee8d3b884a0e78d7180a2
-            </div>
-            <div class=" product-body">
-                <p class="product-category">Agente</p>
-                <h3 class="product-name"><a href="#">{{$agente->nombre}}</a></h3>
-                <div class="valoracion">
-                    <input id="radio1" type="radio" name="estrellas" value="5">
-                    <label for="radio1"><i class="fa fa-star"></i></label>
-                    <input id="radio2" type="radio" name="estrellas" value="4">
-                    <label for="radio2"><i class="fa fa-star"></i></label>
-                    <input id="radio3" type="radio" name="estrellas" value="3">
-                    <label for="radio3"><i class="fa fa-star"></i></label>
-                    <input id="radio4" type="radio" name="estrellas" value="2">
-                    <label for="radio4"><i class="fa fa-star"></i></label>
-                    <input id="radio5" type="radio" name="estrellas" value="1">
-                    <label for="radio5"><i class="fa fa-star"></i></label>
-                </div>
+                            </div>
+                            <div class=" product-body">
+                                <p class="product-category">Agente</p>
+                                <h3 class="product-name"><a href="#">{{$agente->nombre}}</a></h3>
+                                <div class="valoracion">
+                                    <input id="radio1" type="radio" name="estrellas" value="5">
+                                    <label for="radio1"><i class="fa fa-star"></i></label>
+                                    <input id="radio2" type="radio" name="estrellas" value="4">
+                                    <label for="radio2"><i class="fa fa-star"></i></label>
+                                    <input id="radio3" type="radio" name="estrellas" value="3">
+                                    <label for="radio3"><i class="fa fa-star"></i></label>
+                                    <input id="radio4" type="radio" name="estrellas" value="2">
+                                    <label for="radio4"><i class="fa fa-star"></i></label>
+                                    <input id="radio5" type="radio" name="estrellas" value="1">
+                                    <label for="radio5"><i class="fa fa-star"></i></label>
+                                </div>
 
-                <!--<div class="product-btns">
+                                <!--<div class="product-btns">
                     <button class="add-to-wishlist"><i class="fa fa-heart-o"></i></button>
                     <button class="add-to-cart"><i class="fa fa-shopping-cart"></i></button>
             </div>-->
-                <br>
-                <i class="fa fa-phone" aria-hidden="true"> {{$agente->telefono}}</i>
-                <br>
-                <i class="glyphicon glyphicon-envelope"> {{$agente->email}}</i>
+                                <br>
+                                <i class="fa fa-phone" aria-hidden="true"> {{$agente->telefono}}</i>
+                                <br>
+                                <i class="glyphicon glyphicon-envelope"> {{$agente->email}}</i>
 
 
-                <h4 class="product-price">
-                    <!--
+                                <h4 class="product-price">
+                                    <!--
                     <div class="container">
                         <div class="middle">
                             <div class="text">John Doe</div>
                         </div>
                     </div>
                     -->
-                    Ver mas
-                </h4>
+                                    Ver mas
+                                </h4>
 
-            </div>
-        </div>
-    </div>
-    @endforeach
-<body>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+
+                    <body>
 
 
 
 </html>
 
-<<<<<<< HEAD
-<style>
+<<<<<<< HEAD <style>
     .middle {
-        transition: .5s ease;
-        opacity: 0;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%)
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%)
     }
 
 
 
     .container:hover .middle {
-        opacity: 1;
+    opacity: 1;
     }
 
     .text {
-        background-color: #4CAF50;
-        color: white;
-        font-size: 16px;
-        padding: 20px 2px;
+    background-color: #4CAF50;
+    color: white;
+    font-size: 16px;
+    padding: 20px 2px;
     }
-</style>
-=======
-<script>
-    $(document).ready(function() {
-        var zindex = 10;
+    </style>
+    =======
+    <script>
+        $(document).ready(function() {
+            var zindex = 10;
 
-        $("div.card").click(function(e) {
-            e.preventDefault();
+            $("div.card").click(function(e) {
+                e.preventDefault();
 
-            var isShowing = false;
+                var isShowing = false;
 
-            if ($(this).hasClass("show")) {
-                isShowing = true
-            }
+                if ($(this).hasClass("show")) {
+                    isShowing = true
+                }
 
-            if ($("div.cards").hasClass("showing")) {
-                // a card is already in view
-                $("div.card.show")
-                    .removeClass("show");
+                if ($("div.cards").hasClass("showing")) {
+                    // a card is already in view
+                    $("div.card.show")
+                        .removeClass("show");
 
-                if (isShowing) {
-                    // this card was showing - reset the grid
-                    $("div.cards")
-                        .removeClass("showing");
+                    if (isShowing) {
+                        // this card was showing - reset the grid
+                        $("div.cards")
+                            .removeClass("showing");
+                    } else {
+                        // this card isn't showing - get in with it
+                        $(this)
+                            .css({
+                                zIndex: zindex
+                            })
+                            .addClass("show");
+
+                    }
+
+                    zindex++;
+
                 } else {
-                    // this card isn't showing - get in with it
+                    // no cards in view
+                    $("div.cards")
+                        .addClass("showing");
                     $(this)
                         .css({
                             zIndex: zindex
                         })
                         .addClass("show");
 
+                    zindex++;
                 }
 
-                zindex++;
-
-            } else {
-                // no cards in view
-                $("div.cards")
-                    .addClass("showing");
-                $(this)
-                    .css({
-                        zIndex: zindex
-                    })
-                    .addClass("show");
-
-                zindex++;
-            }
-
+            });
         });
-    });
-</script>
->>>>>>> 8669d520c1228981195ee8d3b884a0e78d7180a2
+    </script>
+    >>>>>>> 8669d520c1228981195ee8d3b884a0e78d7180a2
